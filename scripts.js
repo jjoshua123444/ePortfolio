0ecf069bffbc90b3c6aba358387d561b470acc1e
Joshua James <jjoshua123444@gmail.com>
2024-04-21 14:48:29 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("scroll", function() {
-    var button = document.getElementById("back-to-top-button");
-    if (window.pageYOffset > 300) {
-        button.style.display = "block";
-    } else {
-        button.style.display = "none";
-    }
-});
